b6c1b2f4e0007423490c111aa449f5082681e4b3
gxy <771761353@qq.com>
2018-02-04 14:04:14 +0800

    “start”

--- JavaSrc001/src/src001/javasrc001.java
+++ JavaSrc001/src/src001/javasrc001.java
@@ -3,9 +3,7 @@ package src001;
 public class javasrc001 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hello github");
-		System.out.println("hello github*%%");
+		// TODO Auto-generated method stub		
 	
 	}
 
