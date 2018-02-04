231c598790cc35cd920ba1f801d801afb398abff
gxy <771761353@qq.com>
2018-02-04 13:56:33 +0800

    /

--- JavaSrc001/src/src001/javasrc001.java
+++ JavaSrc001/src/src001/javasrc001.java
@@ -5,7 +5,7 @@ public class javasrc001 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello github");
-		System.out.println("hello github**");
+		System.out.println("hello github*");
 	
 	}
 
